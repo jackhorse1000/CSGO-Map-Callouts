5c8c2d79b52954af7969c7f479945cad9b2b15a3
jackhorsburgh <jackhorse1000@gmail.com>
2018-01-26 20:55:00 +0000

    added ad banner to comply with google ads

--- app/src/main/java/com/jshdev/jack/csgo_map_callouts/OverpassActivity.java
+++ app/src/main/java/com/jshdev/jack/csgo_map_callouts/OverpassActivity.java
@@ -4,15 +4,14 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.ImageView;
 
-import com.google.android.gms.ads.AdListener;
 import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
 import com.google.android.gms.ads.InterstitialAd;
 
 import uk.co.senab.photoview.PhotoViewAttacher;
 
 public class OverpassActivity extends AppCompatActivity {
-    InterstitialAd mInterstitialAd;
-    private InterstitialAd interstitial;
+    private AdView mAdView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,29 +24,10 @@ public class OverpassActivity extends AppCompatActivity {
         photoView.update();
 
 
+        mAdView = (AdView) findViewById(R.id.overpassAdView);
         AdRequest adRequest = new AdRequest.Builder().build();
-
-
-        // Prepare the Interstitial Ad
-        interstitial = new InterstitialAd(OverpassActivity.this);
-        // Insert the Ad Unit ID
-        interstitial.setAdUnitId(getString(R.string.admob_interstitial_id));
-
-        interstitial.loadAd(adRequest);
-        // Prepare an Interstitial Ad Listener
-        interstitial.setAdListener(new AdListener() {
-            public void onAdLoaded() {
-                // Call displayInterstitial() function
-                displayInterstitial();
-            }
-
-        });
+        mAdView.loadAd(adRequest);
     }
 
-    private void displayInterstitial() {
-// If Ads are loaded, show Interstitial else show nothing.
-        if (interstitial.isLoaded()) {
-            interstitial.show();
-        }
-    }
+
 }
